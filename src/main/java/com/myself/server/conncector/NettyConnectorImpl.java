b0e0f35cbe6201123c9e8000fdd0f9720d2af69e
ChenMinHuang <cheneyhuang81@gmail.com>
2016-12-17 14:02:07 +0800

    add new map cache

--- src/main/java/com/myself/server/conncector/NettyConnectorImpl.java
+++ src/main/java/com/myself/server/conncector/NettyConnectorImpl.java
@@ -1,5 +1,45 @@
 package com.myself.server.conncector;
 
+import com.myself.server.conncector.handler.TripleColourLightHandler;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 public class NettyConnectorImpl implements SocketConnector{
-	
+    public void bind(int port) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            // 配置服务器的NIO线程租
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(new ChildChannelHandler());
+
+            // 绑定端口，同步等待成功
+            ChannelFuture f = b.bind(port).sync();
+            // 等待服务端监听端口关闭
+            f.channel().closeFuture().sync();
+        } finally {
+            // 优雅退出，释放线程池资源
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+        @Override
+        protected void initChannel(SocketChannel arg0) throws Exception {
+            System.out.println("server initChannel..");
+            arg0.pipeline().addLast(new TripleColourLightHandler());
+        }
+    }
+    
 }
